fbc30d2329e0b9117c7aa9590a308c5da5fc445a
Vitya Knyazev <Utopio@tut.by>
2023-12-20 22:15:02 +0300

    Added controller layer, context listener for initialization app via common context. Added controller realization for person CRUD operations: get, post, put, delete. Added government controller realization that returns pdf file data of all person government services by person id.

---
+++ src/main/java/ru/clevertec/knyazev/controller/GovernmentController.java
@@ -0,0 +1,66 @@
+package ru.clevertec.knyazev.controller;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.MimeTypeUtils;
+import ru.clevertec.knyazev.config.AppContextListener;
+import ru.clevertec.knyazev.pdf.exception.PDFDocumentException;
+import ru.clevertec.knyazev.service.GovernmentService;
+import ru.clevertec.knyazev.service.exception.ServiceException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@Slf4j
+@WebServlet(value = "/services")
+public class GovernmentController extends HttpServlet {
+    private static final String PERSON_ID_REQUEST_PARAM = "person_id";
+
+    private static final String EMPTY_JSON = "{}";
+
+    private GovernmentService governmentServiceImpl;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        governmentServiceImpl = (GovernmentService) config.getServletContext()
+                .getAttribute(AppContextListener.GOVERNMENT_SERVICE_IMPL);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String personIdParam = req.getParameter(PERSON_ID_REQUEST_PARAM);
+
+        try {
+            UUID personId = UUID.fromString(personIdParam);
+
+            String pdfLink = governmentServiceImpl.getAbsolutePathByPersonId(personId);
+
+            try (InputStream is = new FileInputStream(pdfLink)) {
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+                resp.setContentType("application/pdf");
+                resp.getOutputStream().write(is.readAllBytes());
+            }
+
+        } catch (IllegalArgumentException | FileNotFoundException |
+                 SecurityException | PDFDocumentException | ServiceException e) {
+            log.error(e.getMessage(), e);
+
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            resp.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
+            resp.getWriter().append(EMPTY_JSON);
+        }
+    }
+}
